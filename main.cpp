864bddc014172371ee5332ef6d4e464b61650a2f
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-10-02 18:52:52 +0200

    a large graph

--- main.cpp
+++ main.cpp
@@ -18,40 +18,26 @@ int main()
 //   
 
   ListDigraph g;
-  ListDigraph::Node a = g.addNode();
-  ListDigraph::Node b = g.addNode();
-  ListDigraph::Node c = g.addNode();
-  ListDigraph::Node d = g.addNode();
-  ListDigraph::Node e = g.addNode();
-  ListDigraph::Node f = g.addNode();
-
-  ListDigraph::Arc  ab = g.addArc(a, b);
-  ListDigraph::Arc  ac = g.addArc(a, c);
-  ListDigraph::Arc  bd = g.addArc(b, d);
-  ListDigraph::Arc  ce = g.addArc(c, e);
-  ListDigraph::Arc  df = g.addArc(d, f);
-  ListDigraph::Arc  ef = g.addArc(e, f);
-
   ListDigraph::ArcMap<int> capacity(g);
-  capacity[ab] = 30;
-  capacity[bd] = 20;
-  capacity[df] = 30;
-  capacity[ac] = 20;
-  capacity[ce] = 30;
-  capacity[ef] = 20;
 
-  Bfs<ListDigraph> bfs(g);
-  bfs.run(a);
+  int LEVELS = 10000000;
 
-  Preflow<ListDigraph> preflow(g, capacity, a, f);
+  ListDigraph::Node s = g.addNode();
+  ListDigraph::Node t = g.addNode();
+
+  for(int i = 0; i < LEVELS; i++) {
+	  ListDigraph::Arc a = g.addArc(s, t);
+	  capacity[a] = 1;
+  }
+  cout << "Algo" << endl;
+
+  Preflow<ListDigraph> preflow(g, capacity, s, t);
   preflow.run();
 
   cout << "Hello World! This is LEMON library here." << endl;
   cout << "We have a directed graph with " << countNodes(g) << " nodes "
        << "and " << countArcs(g) << " arc." << endl;
-  int count= countOutArcs(g, a);
 
-  cout << "Node u has degree " << count << endl;
   cout << "maxFlow " << preflow.flowValue() << endl;
   return 0;
 }
