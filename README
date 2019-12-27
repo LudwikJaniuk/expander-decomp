68e070094069645ae23fb013235daeb5d5284a11
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-12-28 00:36:55 +0100

    write your name on it

---
+++ README
@@ -0,0 +1,5 @@
+Authored by LUDVIG JANIUK
+
+Compilation instructions can be inferred from CmakeLists.txt
+run with --help for run instructions
+
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Authored by Ludvig Janiuk 2019 as part of individual project at KTH.
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cert-msc32-c"
 #pragma ide diagnostic ignored "cppcoreguidelines-slicing"
